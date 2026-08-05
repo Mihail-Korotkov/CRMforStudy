ac714256472917b6152c9469965c07fa90d0a965
Mihail-Korotkov <mixail.korotkov.01@bk.ru>
2026-08-05 10:58:13 +0300

    Datatabase conected, superuser created

--- CRM/settings.py
+++ CRM/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'CRM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'databaseforcrm',
+        'USER': 'misha',
+        'PASSWORD': 'mishana',
+        'HOST': 'localhost',
+        'PORT': '5433',
+
     }
 }
 
